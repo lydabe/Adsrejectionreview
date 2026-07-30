2ae4a840eb8a64c1b9d3ff1606ab101a9a1cc9ce
lydabe <lydabe@163.com>
2026-07-29 17:35:47 -0700

    Add GitHub Pages site configuration

---
+++ pages-entry.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ViolationAnalyzer from "./app/ViolationAnalyzer";
+import "./app/globals.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ViolationAnalyzer />
+  </React.StrictMode>,
+);
